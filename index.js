af80b3dfd3cf3e3602aec51fdbcd89f9de726ec9
dahiya001rohit <rohit830770@gmail.com>
2026-06-23 11:16:51 +0530

    made changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import express from "express";
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
